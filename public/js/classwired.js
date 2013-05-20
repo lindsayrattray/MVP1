a3279f2ff2be75fea66b5b783df425143f0ca7dd
Kav Singh <flakisms@gmail.com>
2013-05-20 21:08:51 +1000

    Sudden terror

--- public/js/classwired.js
+++ public/js/classwired.js
@@ -1,21 +1,96 @@
-(function($){
+var classwired = (function(cw) {
 
-    $("#register-form").on('submit', function(e) {
-        e.preventDefault();
-        submitEmail();
-    });
+    cw.reg_form = $("#register-form");
 
-    function submitEmail() {
+    cw.points = $("#info .point-container");
+    cw.points_nav = $("#info .point-nav");
+    cw.curr_point_index = 0;
+
+    cw.init = function() {
+        this.setupRegForm();
+        this.setupPoints();
+    }
+
+    cw.setupRegForm = function() {
+        var self = this;
+
+        this.reg_form.on('submit', function(e) {
+            console.log('hi');
+            e.preventDefault();
+            self.submitEmail();
+        });
+    }
+
+    cw.submitEmail = function() {
         $.ajax({
             url: 'app/register',
             method: 'post',
-            data: $('#register-form').serialize(),
+            data: this.reg_form.serialize(),
             context: this
         }).done(function(response){
-            alert('Thanks for registering!');
+            this.onRegSuccess(response);
         }).fail(function(response){
-            alert('Sorry, there was an error. Please try again soon.');
+            this.onRegFail(response);
         });
     }
+
+    cw.onRegSuccess = function(response) {
+        alert('Thanks for registering!');
+    }
+
+    cw.onRegFail = function(response) {
+        alert('Sorry, there was an error. Please try again soon.');
+    }
+
+    cw.setupPoints = function() {
+        this.point_pages = this.points.find('.point').get();
+        this.point_nav_buttons = this.points_nav.find('.point-nav-indicator').get();
+        var self = this;
+
+        var bindClick = function(item, index) {
+            $(item).on('click', function() {
+                self.gotoPoint(index);
+            });
+        }
+
+        _.map(this.point_nav_buttons, bindClick);
+
+        this.gotoPoint(0);
+
+    }
+
+    cw.gotoPoint = function(atindex) {
+        $(this.point_nav_buttons[this.curr_point_index]).removeClass('current');
+        this.curr_point_index = atindex;
+        var num_indices = this.point_pages.length - 1;
+        
+        _.map(this.point_pages, function(item, index) {
+            var newleft = (index - atindex) * 100;
+            $(item).css('left', newleft+'%');
+        });
+
+        $(this.point_nav_buttons[this.curr_point_index]).addClass('current');
+    }
+
+    cw.goNextPoint = function() {
+        var next = Math.min(this.point_pages.length - 1, this.curr_point_index + 1);
+
+        this.gotoPoint(next);
+    }
+
+    cw.goPreviousPoint = function() {
+        var next = Math.max(0, this.curr_point_index - 1);
+
+        this.gotoPoint(next);
+    }
+
+    return cw;
+
+})(classwired || {});
+
+
+(function($){
+
+    classwired.init();
 
 })(jQuery);
