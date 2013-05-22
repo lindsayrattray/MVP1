7e995f68730809f9ef60e8c0aa79910271127d7c
Lindsay Rattray <lindsayrattray@yahoo.com>
2013-05-22 13:51:51 +1000

    another attempt at Pony.Mail settings

--- classwired.rb
+++ classwired.rb
@@ -19,16 +19,15 @@ post '/app/register' do
         :subject => 'ClassWired registration',
         :body => params[:email],
         :from => 'lindsayrattray@yahoo.com',
-        :port => '587',
         :via => :smtp,
-        :via_options => { 
-            :address              => 'smtp.sendgrid.net', 
-            :port                 => '587', 
-            :enable_starttls_auto => true, 
-            :user_name            => ENV['SENDGRID_USERNAME'], 
-            :password             => ENV['SENDGRID_PASSWORD'], 
-            :authentication       => :plain, 
-            :domain               => ENV['SENDGRID_DOMAIN']
+        :via_options => {
+            :address => 'smtp.sendgrid.net',
+            :port => '587',
+            :domain => 'heroku.com',
+            :user_name => ENV['SENDGRID_USERNAME'],
+            :password => ENV['SENDGRID_PASSWORD'],
+            :authentication => :plain,
+            :enable_starttls_auto => true
         }
     )
 
