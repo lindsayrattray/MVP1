115ea2003e777e85e3a9925362a68f60503c68c8
Lindsay Rattray <lindsayrattray@yahoo.com>
2013-06-25 13:05:46 +1000

    test if params[:email] is the problem

--- classwired.rb
+++ classwired.rb
@@ -28,20 +28,20 @@ post '/app/register' do
         delivery_method :smtp, options
     end
 
-    recipient = params[:email] # this is bizarrely necessary
+    recipient = "lindsayrattray@yahoo.com" # test if this is the problem params[:email] # this is bizarrely necessary
 
     begin
-        mailObject = Mail.deliver do
-            from 'classwired@gmail.com'
-            to 'lindsayrattray@yahoo.com'
-            subject 'ClassWired registration'
-            body recipient
-        end
+            mailObject = Mail.deliver do
+                from 'classwired@gmail.com'
+                to 'lindsayrattray@yahoo.com'
+                subject 'ClassWired registration'
+                body recipient
+            end
 
         content_type :json
         { sent: 'success' }.to_json
 
-    rescue
+        rescue
         halt 500
     end
 end
